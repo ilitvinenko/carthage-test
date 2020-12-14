5581a8cd5e6029f6a6bace76c82d5e209d58f351
Igor Litvinenko <igor.litvinenko@airship.com>
2020-12-14 19:49:39 +0200

    updated checksum

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "CarthageTestLib",
             url: "https://github.com/ilitvinenko/carthage-test/releases/download/v0.1.3/CarthageTestLib.framework.zip",
-            checksum: "f009123b6ac8b9c60538ea2eaaf41f6f53a9020071f5e2e71f45aab941852726"
+            checksum: "f7da1617fb101d77b44c39eed4998fefdc523b05e39bcb4b827dad8f4fcf2d04"
         )
     ]
 )
