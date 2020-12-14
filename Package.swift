72b421a9beb0ae059ff12a2ce931b34a80c0568f
Igor Litvinenko <igor.litvinenko@airship.com>
2020-12-14 21:06:06 +0200

    updated url

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CarthageTestLib",
-            url: "https://github.com/ilitvinenko/carthage-test/releases/download/v0.1.3/CarthageTestLib.framework.zip",
+            url: "https://github.com/ilitvinenko/carthage-test/releases/download/v0.1.4/CarthageTestLib.xcframework.zip",
             checksum: "b0112505830eb8d6c680526c393ae3ae49f8d53e3df9ede2e7a8b8289937a2d7"
         )
     ]
